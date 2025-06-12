6890a7c10d0120d5c92b5417e31612ce84902075
janamucci18 <janamucci18@gmail.com>
2025-06-12 18:25:10 -0400

    initial set up

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodaysMeals from './FrontEnd/TodaysMeals';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Macro Tracker</h1>
+      <TodaysMeals />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FrontEnd/TodaysMeals.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TodaysMeals = () => {
+  return (
+    <div  style={{ color: 'black' }}>
+      <h2>Today’s Meals</h2>
+      <p>No meals logged yet.</p>
+    </div>
+  );
+};
+
+export default TodaysMeals;
